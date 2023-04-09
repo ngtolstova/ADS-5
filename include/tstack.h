ffe44328c8259c16da2636fc839eb8d3eef29965
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-04-09 13:49:19 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -6,41 +6,42 @@
 template <typename T, int size>
 class TStack {
   // добавьте код стека
-private:
-  T arr[size] = {0};
-  int top = 0;
-public:
-  TStack(): top(-1) {}
-  void push(const T& value) {
-    if (isFull())
-      throw std::string("Full");
-    else
-      arr[++top] = value;
-   }
-   const T& pop() {
-     if (isEmpty())
-       throw std::string("Empty");
+ private:
+   T arr[size] = {0};
+   int top = 0;
+  
+ public:
+   TStack(): top(-1) {}
+   void push(const T& value) {
+     if (isFull())
+       throw std::string("Full");
      else
-       return arr[top--];
-   }
-   const T& get() const {
-     if (isEmpty())
-       throw std::string("Empty");
-     else
-       return arr[top];
-   }
-   bool isEmpty() const {
-     return top == -1;
-   }
-   bool isFull() const {
-     return top == size - 1;
-   }
-   const T& GetArr(int i) {
-     return arr[i];
-   }
-   int GetTop() {
-     return top;
-   }
+       arr[++top] = value;
+    }
+    const T& pop() {
+      if (isEmpty())
+        throw std::string("Empty");
+      else
+        return arr[top--];
+    }
+    const T& get() const {
+      if (isEmpty())
+        throw std::string("Empty");
+      else
+        return arr[top];
+    }
+    bool isEmpty() const {
+      return top == -1;
+    }
+    bool isFull() const {
+      return top == size - 1;
+    }
+    const T& GetArr(int i) {
+      return arr[i];
+    }
+    int GetTop() {
+      return top;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
