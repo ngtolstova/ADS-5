4e466b32d1cae88c4fd500d6a4463260afc38018
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-04-09 17:53:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,72 +14,79 @@ TStack <int, size> val;
 std::string Func1(unsigned char c, int* j, std::string output);
 std::string infx2pstfx(std::string inpt) {
   std::size_t len = inpt.length();
-  std::string output(len, '\0');
-  unsigned char c , tp = '\0';
-  int j = 0;
-  for (int i = 0; i < len; i++) {
-    c = inpt[i];
-    try {
-      switch (c) {
-        case '(':
-          ts.push(c);
-          break;
-        case ')':
-          tp = '\0';
-          while (tp != '(') {
-            tp = ts.pop();
-            if (tp!= '(') {
-              output[j] = tp;
-              j++;
-            } else {
-                break;
-            }
-          }
-          break;
-        case '+':
-          output = Func1(c, &j, output);
-          break;
-        case '-':
-          output = Func1(c, &j, output);
-          break;
-        case '*':
-          output = Func1(c, &j, output);
-          break;
-        case '/':
-          output = Func1(c, &j, output);
-          break;
-        default:
-          if (c >= '0' && c <= '9') {
-            output[j] = c;
-            j++;
-          }
-      }
-    }
-    catch (std::string e) {
-      std::cout << e << std::endl;
-    }
-  }
-  tp = '\0';
-  while (ts.GetTop() >= 0) {
-    tp = ts.pop();
-    if (tp != '(') {
-      output[j] = tp;
-      j++;
-    }
-  }
-  return output;
+	std::string output, buf;
+	unsigned char c , tp = '\0';
+	for (int i = 0; i < len; i++)	{
+		c = inpt[i];
+		try {
+			switch (c) {
+			case '(':
+				ts.push(c);
+				break;
+			case ')':
+				tp = '\0';
+				while (tp != '(')	{
+					tp = ts.pop();
+					if (tp != '(') {
+						output += tp;
+						output += " ";
+						std::cout << tp << " ";
+					}	else {
+            break;
+          } 
+				}
+				break;
+			case '+':
+				buf = "";
+				output += Func1(c, buf);
+				break;
+			case '-':
+				buf = "";
+				output += Func1(c, buf);
+				break;
+			case '*':
+				buf = "";
+				output += Func1(c, buf);
+				break;
+			case '/':
+				buf = "";
+				output += Func1(c, buf);
+				break;
+			default:
+				if (c >= '0' && c <= '9') {
+					output += c;
+					output += " ";
+					std::cout << c << " ";
+        }
+			}
+		}
+		catch (std::string e)	{
+			std::cout << e << std::endl;
+		}
+	}
+	tp = '\0';
+	while (!ts.isEmpty()) {
+		tp = ts.pop();
+		if (tp != '(') {
+			output += tp;
+			output += " ";
+			std::cout << tp << " ";
+		}
+	}
+	return output;
 }
-std::string Func1(unsigned char c, int* j, std::string outp) {
+std::string Func1(unsigned char c, std::string outp) {
   char temp;
-  if (ts.GetTop() >= 0) {
-    temp = ts.get();
-    while (ts.GetTop() >= 0 && (Priority(temp) >= Priority(c))) {
-      outp[*j] = ts.pop();
-      (*j)++;
-    }
-  }
-  ts.push(c);
-  return outp;
+  if (!ts.isEmpty()) {
+		temp = ts.get();
+		while (!ts.isEmpty() && (Priority(temp) >= Priority(c))) {
+			outp += ts.pop();
+			outp += " ";
+			std::cout << outp;
+		}
+	}
+	ts.push(c);
+	return outp;
 }
 int Priority(char c) {
   int pr = 0;
@@ -104,24 +111,23 @@ int Priority(char c) {
 }
 int eval(std::string post) {
   std::size_t len = post.length();
-  int count = 0, z;
+	int count = 0, z;
+	std::string N;
   char c;
   for (int i = 0; i < len; i++) {
-    c = post[i];
-    if (c == '\0') {
-      break;
-    }
+		c = post[i];
+    if (c == ' ') continue;
     if (c >= '0' && c <= '9') {
-      z = atoi(&c);
-      val.push(z);
-    } else {
-      int second = val.pop();
-      int first = val.pop();
-      z = Execute(c, first, second);
-      val.push(z);
-    }
-  }
-  return val.pop();
+			z = atoi(&c);
+			val.push(z);
+		} else {
+			int second = val.pop();
+			int first = val.pop();
+			z = Execute(c, first, second);
+			val.push(z);
+		}
+	}
+	return val.pop();
 }
 int Execute(char oper, int first, int second) {
   switch (oper) {
