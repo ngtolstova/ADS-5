424a78251de423572e10d877f1cd39f49dbdbc26
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-04-09 13:28:52 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,15 +1,17 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
   private:
     T arr[size] = {0};
     int top = 0;
+
   public:
     TStack(): top(-1) {}
-    void push(T& value) {
+    void push(const T& value) {
       if (isFull())
        throw std::string("Full");
       else
